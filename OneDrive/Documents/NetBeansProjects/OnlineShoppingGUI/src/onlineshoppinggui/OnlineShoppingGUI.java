359b47edca5371471b420df94db7c718b63917ae
Johnny Set <ztk7656@autuni.ac.nz>
2024-06-10 16:53:44 +1200

    First Commit

--- OneDrive/Documents/NetBeansProjects/OnlineShoppingGUI/src/onlineshoppinggui/OnlineShoppingGUI.java
+++ OneDrive/Documents/NetBeansProjects/OnlineShoppingGUI/src/onlineshoppinggui/OnlineShoppingGUI.java
@@ -14,7 +14,7 @@ public class OnlineShoppingGUI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Test");
+        // TODO code application logic here
     }
     
 }
